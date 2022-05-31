f4906e8d614421e5bc9aa9bf545576eb1324c1f3
nahtandg <nathan.silva@padolabs.com.br>
2022-05-31 17:18:42 -0300

    backend

---
+++ backend/src/routes/Produtos.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const router = express.Router()
+
+
+let dummyCount = 0;
+let Produtos = []
+
+//salvar pedido
+router.get('/', (req, res) => {
+    res.status(200).send(Produtos);
+})
+
+//requisição adicionar dados de Pedido
+router.post('/', (req, res) => {
+    
+    const request = req.body;
+
+    //created obj
+    const produtoObj = {
+
+        id: dummyCount += 1,
+        Nome: request.nome,
+        LinkImg: request.LinkImg,
+        description: request.description
+
+    }
+    
+    Produtos.push(produtoObj);
+    res.status(201).send();
+})
+
+router.get('/', (req, res) => {
+    res.send(Produtos);
+})
+
+
+module.exports = router;
