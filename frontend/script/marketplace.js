113c87e55a97b25f16eda32ceb5787a824373a3d
nahtandg <nathan.silva@padolabs.com.br>
2022-06-14 14:13:50 -0300

    funcionando sem mongoose

--- frontend/script/marketplace.js
+++ frontend/script/marketplace.js
@@ -1,15 +1,3 @@
-// const response = [
-//   {
-//     nome: "Bosta em Lata",
-//     img: "https://static3.tcdn.com.br/img/img_prod/350075/adubo_organico_bosta_em_lata_para_orquideas_400g_5619_1_20220412114051.jpg",
-//     preco: 50,
-//   },
-//   {
-//     nome: "Adubo",
-//     img: "https://www.sitiopema.com.br/wp-content/uploads/2021/09/Adubo-organico-macrofitas-12.jpg",
-//     preco: 100,
-//   },
-// ];
 
 const url = "http://localhost:4000/produtos";
 let response = []
@@ -26,18 +14,10 @@ function getUsers() {
 }
 
 getUsers();
+setTimeout(teste, 300);
 
-let httpRequest;
+function teste(){
 
-if (window.XMLHttpRequest) {
-  // Mozilla, Safari, ...
-  httpRequest = new XMLHttpRequest();
-} else if (window.ActiveXObject) {
-  // IE 8 and older
-  httpRequest = new ActiveXObject("Microsoft.XMLHTTP");
-}
-
-httpRequest.onreadystatechange = window.onload = () => {
   const cardRender = document.getElementById("render");
 
   function createdCardHtml(Nome, Img, Preco) {
@@ -58,15 +38,8 @@ httpRequest.onreadystatechange = window.onload = () => {
     return anterior + createdCardHtml(atual.Nome, atual.Img, atual.Preco);
   }, "");
 
-  // const cards = response.map((res) => {
-
-  //   return createdCardHtml(res.nome, res.img, res.description);
-
-  // }).join("")
-
-  console.log(cards);
-
   cardRender.innerHTML = cards;
-
   console.log(cardRender);
-};
+
+}
+
