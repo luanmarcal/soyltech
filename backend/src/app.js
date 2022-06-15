9a3b58343318f8f629a11aeffa48274641550f21
nahtandg <nathan.silva@padolabs.com.br>
2022-06-15 12:54:27 -0300

    working with mongoose

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const cors = require('cors')
-
+const mongoose = require('mongoose')
 
 const server = express()
 const port = 4000
@@ -17,6 +17,13 @@ server.use('/pedidos', pedidosRoute);
 server.use('/produtos', produtosRoute);
 
 
-server.listen(port, () => {
-    console.log(`server running on port ${port}`)
-})
+//servidor será iniciado se a conexão com o DataBase, for aceita;
+const main = async () => {
+    await mongoose.connect(`mongodb+srv://${process.env.MY_USER}:${process.env.MY_PASSWORD}@cluster0.mxpes.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`);
+}
+
+main().then(() => {
+    server.listen(port, () => {
+        console.log(`server running on port ${port}`)
+    })
+}).catch(err => console.log(err));
